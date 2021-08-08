d8c6b07ba1022e58e8e3f5f10e29a98390479eb4
astika <grgastika@gmail.com>
2021-08-09 01:19:25 +0545

    initial

--- Settings_page.py
+++ Settings_page.py
@@ -1,30 +1,37 @@
 from tkinter import *
 
-sett = Tk()
-sett.geometry('1280x720')
-sett.title('G-Pass-Setting')
-sett.iconbitmap('Images/G-pass_ico.ico')
-sett.resizable(False, False)  # stop the window from resizing
+logsin = Tk()
+logsin.geometry('1280x720')
+logsin.resizable(False, False)  # stop the window from resizing
 
-sbg = PhotoImage(file='Images/Backgroundin.png')
-Label(sett, image=sbg, bg='#855700').place(x=-3, y=-3)
+settings_frame = LabelFrame(logsin, width='1280', height='720', )
+settings_frame.place(x=0, y=0)
 
-sf = PhotoImage(file='Images/Setting Frame.png')
-Label(sett, image=sf, bg='#855700').place(x=26, y=20)
+sett = Label(settings_frame, text='Settings', bg='#5d5a5a', fg='#06ebb4')
+sett.place(x='748', y='47')
 
-sh = PhotoImage(file='Images/Settings.png')
-Label(sett, image=sh, bg='#5D5A5A').place(x=784, y=45)
+bg_image = PhotoImage(file='Images/Background.png')
+bg_img = Label(settings_frame, image=bg_image, )
+bg_img.place(x=0, y=0)
 
-settf = PhotoImage(file='Images/Settings Folder.png')
-Button(sett, image=settf, bg='#C4C4C4', bd=0, activebackground='#C4C4C4').place(x=71, y=120)
+back = PhotoImage(file='Images/Setting Frame.png')
+back_img = Label(settings_frame, image=back, bg='#855700')
+back_img.place(x='26', y='20')
 
-settp = PhotoImage(file='Images/Settings Change password.png')
-Button(sett, image=settp, bg='#C4C4C4', bd=0, activebackground='#C4C4C4').place(x=71, y=256)
+elog = PhotoImage(file="Images/Settings Export.png")
+e_logo = Label(settings_frame, image=elog, bg='#c4c4c4')
+e_logo.place(x='71', y='392')
 
-sette = PhotoImage(file='Images/Settings Export.png')
-Button(sett, image=sette, bg='#C4C4C4', bd=0, activebackground='#C4C4C4').place(x=71, y=392)
+cplog = PhotoImage(file='Images/Settings Change Password.png')
+cp_logo = Label(settings_frame, image=cplog, bg='#c4c4c4')
+cp_logo.place(x='71', y='256')
 
-settl = PhotoImage(file='Images/Settings Logout.png')
-Button(sett, image=settl, bg='#C4C4C4', bd=0, activebackground='#C4C4C4').place(x=71, y=529)
+aflog = PhotoImage(file='Images/Settings Folder.png')
+fol_logo = Label(settings_frame, image=aflog, bg='#c4c4c4')
+fol_logo.place(x='71', y='120')
 
-mainloop()
+lolog = PhotoImage(file='Images/Settings Logout.png')
+lo_logo = Label(settings_frame, image=lolog, bg='#c4c4c4')
+lo_logo.place(x='71', y='529')
+
+logsin.mainloop()
