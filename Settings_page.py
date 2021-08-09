98f3bc59e9f2e474a117fa28adf0347e36b27c9a
astika <grgastika@gmail.com>
2021-08-09 15:13:46 +0545

    Settings

--- Settings_page.py
+++ Settings_page.py
@@ -1,47 +1,95 @@
 from tkinter import *
 
-logsin = Tk()
-logsin.geometry('1280x720')
-logsin.resizable(False, False)
-# stops the window from resizing
+settings = Tk()
+settings.geometry('1280x720')
+settings.resizable(False, False)
 
 
-def settings():
-''' function fot the settings menu'''
+def logout():
+    '''function to logout of the password manager'''
 
-    global sett,bg_image,back,elog,cplog,aflog,lolog
-    settings_frame = LabelFrame(logsin, width='1280', height='720', )
-    settings_frame.place(x=0, y=0)
+    logout_frame = LabelFrame(settings_frame, width='744',
+                              height='552', bg='red')
+    logout_frame.place(x='490', y='129')
 
-    sett = Label(settings_frame, text='Settings', bg='#5d5a5a', fg='#06ebb4')
-    sett.place(x='748', y='47')
+    lo_text = Label(logout_frame,
+                    text='Are you sure you want to logout?',
+                    font='Arial, 30')
+    lo_text.place(x='124', y='170')
 
-    bg_image = PhotoImage(file='Images/Background.png')
-    bg_img = Label(settings_frame, image=bg_image, )
-    bg_img.place(x=0, y=0)
+    lo_confirm = Button(logout_frame, text='Logout')
+    lo_confirm.place(x='298', y='379')
 
-    back = PhotoImage(file='Images/Setting Frame.png')
-    back_img = Label(settings_frame, image=back, bg='#855700')
-    back_img.place(x='26', y='20')
 
-    # defining and placing the buttons
+def changepassword():
+    '''
+    function to change the master key/ main password
+    '''
 
-    elog = PhotoImage(file="Images/Settings Export.png")
-    e_logo = Label(settings_frame, image=elog, bg='#c4c4c4')
-    e_logo.place(x='71', y='392')
+    changepassword_frame = LabelFrame(settings_frame, width='744',
+                                      height='552', bg='red')
+    changepassword_frame.place(x='490', y='129')
 
-    cplog = PhotoImage(file='Images/Settings Change Password.png')
-    cp_logo = Label(settings_frame, image=cplog, bg='#c4c4c4')
-    cp_logo.place(x='71', y='256')
+    # String Variables to store new password
+    old_password = StringVar()
+    old_password.set('Old Password')
+    new_password = StringVar()
+    new_password.set('New Password')
+    new_passwordc = StringVar()
+    new_passwordc.set('Confirm New Password')
 
-    aflog = PhotoImage(file='Images/Settings Folder.png')
-    fol_logo = Label(settings_frame, image=aflog, bg='#c4c4c4')
-    fol_logo.place(x='71', y='120')
+    op_entry = Entry(changepassword_frame, text=old_password, font=('Arial', 20), bd=0)
+    op_entry.place(x='148', y='100')
+    np_entry = Entry(changepassword_frame, text=new_password, font=('Arial', 20), bd=0)
+    np_entry.place(x='530', y='291')
+    npc_entry = Entry(changepassword_frame, text=new_password, font=('Arial', 20), bd=0)
+    npc_entry.place(x='530', y='291')
 
-    lolog = PhotoImage(file='Images/Settings Logout.png')
-    lo_logo = Label(settings_frame, image=lolog, bg='#c4c4c4')
-    lo_logo.place(x='71', y='529')
 
-settings()
+global sett, bg_image, back, elog, cplog, aflog, lolog
+settings_frame = LabelFrame(settings, width='1280', height='720')
+settings_frame.place(x=0, y=0)
 
-logsin.mainloop()
+
+bg_image = PhotoImage(file='Images/Background.png')
+bg_img = Label(settings_frame, image=bg_image)
+bg_img.place(x=0, y=0)
+
+back = PhotoImage(file='Images/Setting Frame.png')
+back_img = Label(settings_frame, image=back, bg='#855700')
+back_img.place(x='26', y='20')
+
+
+sett = PhotoImage(file='Images/Settings.png')
+Label(settings_frame,image=sett).place(x=748, y=47)
+# defining and placing the buttons
+
+elog = PhotoImage(file='Images/Settings Export.png')
+e_logo = Button(settings_frame, image=elog, bg='#c4c4c4', bd='0',
+                activebackground='#c4c4c4')
+e_logo.place(x='71', y='392')
+
+cplog = PhotoImage(file='Images/Settings Change Password.png')
+cp_logo = Button(settings_frame, image=cplog, bg='#c4c4c4', bd='0',
+                 activebackground='#c4c4c4',
+                 command=changepassword
+                 )
+cp_logo.place(x='71', y='256')
+
+aflog = PhotoImage(file='Images/Settings Folder.png')
+fol_logo = Button(settings_frame, image=aflog, bg='#c4c4c4', bd='0'
+                  , activebackground='#c4c4c4')
+fol_logo.place(x='71', y='120')
+
+lolog = PhotoImage(file='Images/Settings Logout.png')
+lo_logo = Button(
+    settings_frame,
+    image=lolog,
+    bg='#c4c4c4',
+    bd='0',
+    activebackground='#c4c4c4',
+    command=logout,
+)
+lo_logo.place(x='71', y='529')
+
+settings.mainloop()
