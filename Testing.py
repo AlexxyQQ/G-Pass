6c64d8d73d23016877af3059c8bbb2b69ff89f22
ACER <210220@softwarica.edu.np>
2021-09-13 00:00:06 +0545

    Test# commit

--- Testing.py
+++ Testing.py
@@ -1,18 +1,31 @@
 import pytest
 
+"""
+Testing Login page
+"""
+
 @pytest.fixture
 def tester():
+    """
+    Getting User values and passing on
+    """
     name = "Manjil"
     password = "Password123"
     return (name, password)
 
 
 def test1(tester):
+    """
+    Testing for first name
+    """
     first_name = "Manjil"
     assert tester[0] == first_name
 
 
 def test_2(tester):
+    """
+    Testing Password
+    """
     password_check = "Password"
     assert tester[1] == password_check
 
